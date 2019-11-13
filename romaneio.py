6159f8244b691030a853d321bf11f77f2d6a3e9f
Wesley Castilho <47651529+WesleyGCO@users.noreply.github.com>
2019-11-13 18:52:30 -0300

    Tabela romaneio e suas funções

--- romaneio.py
+++ romaneio.py
@@ -1,7 +1,25 @@
+import peca
+import usuario
+import cliente
+
+
+# Criação da tabela romaneio
 def criar_tab_romaneio(conexao):
     cursor = conexao.cursor()
 
     sql = """
         CREATE TABLE IF NOT EXISTS romaneio (
-            
-        )
+           preco_final INTEGER NOT NULL,
+           dtvenda DATE NOT NULL,
+           idusu INTEGER NOT NULL,
+           idcli INTEGER NOT NULL,
+           idpec INTEGER NOT NULL,
+           FOREIGN KEY (idusu) REFERENCES usuario (rowid),
+           FOREIGN KEY (idcli) REFERENCES cliente (rowid),
+           FOREIGN KEY (idpec) REFERENCES peca (rowid) 
+        );"""
+    
+    
+    cursor.execute(sql)
+
+def criar_romaneio(conexao):
