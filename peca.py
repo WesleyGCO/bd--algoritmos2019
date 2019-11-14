0aa258bc2e43c719ee702a2c7c6e9aa2a578c6f2
Wesley Castilho <47651529+WesleyGCO@users.noreply.github.com>
2019-11-13 22:17:59 -0300

    Tabela peça e suas funções

--- peca.py
+++ peca.py
@@ -6,17 +6,19 @@ def criar_tab_peca(conexao):
         CREATE TABLE peca (
             descricao TEXT NOT NULL,
             quant TEXT NOT NULL,
-            preco INTEGER NOT NULL
+            preco REAL (9,2) NOT NULL
         );"""
 
     cursor.execute(sql)
 
 # Inserção das informações da peça
-def inserir_peca(conexao):
+
+
+def inserir_peca_nova(conexao):
     descricao = input("Descrição da peça: ")
     quant = input("Quantidade: ")
     preco = input("Preço: ")
-    
+
     cursor = conexao.cursor()
 
     sql = """
@@ -26,10 +28,41 @@ def inserir_peca(conexao):
             '{}'
         );""".format(descricao, quant, preco)
 
-
     cursor.execute(sql)
     conexao.commit()
     print("Dados inseridos com sucesso!")
+
+
+def inserir_peca_existente(conexao):
+    cursor = conexao.cursor()
+
+    sql = """
+        SELECT rowid, * FROM peca
+        """
+    cursor.execute(sql)
+
+    listapeca = cursor.fetchall()
+
+    listar_peca(conexao)
+
+    print(" ")
+    id = int(input("Digite o ID da peça que deseja atualização?: "))
+
+    for i in listapeca:
+        if (id == i[0]):
+            print(" ")
+            a = int(input("Quantidade a ser atualizada para mais: "))
+            atual = int(i[2]) + a
+
+            # Atualização quant peças
+            sql = """ UPDATE peca SET quant = {} WHERE rowid = {}
+            """.format(atual, id)
+
+            cursor.execute(sql)
+            conexao.commit()
+
+            print("Atualização efetuada!")
+            print(" ")
 
 
 def listar_peca(conexao):
@@ -38,7 +71,7 @@ def listar_peca(conexao):
     sql = """
         SELECT rowid, * FROM peca
         """
-
+    cursor.execute(sql)
     listapeca = cursor.fetchall()
 
     print("\n\t ===== Listando peças =====")
@@ -48,4 +81,4 @@ def listar_peca(conexao):
         ID: {}
         Descrição: {}
         Quantidade: {}
-        Preço: {}""".format(p[0], p[1], p[2], p[3]))
+        Preço cada unidade: {}""".format(p[0], p[1], p[2], p[3]))
