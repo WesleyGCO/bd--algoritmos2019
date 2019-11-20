d69d0b8e907b3f1d3e25f30627e6821023f46d6f
Wesley Castilho <47651529+WesleyGCO@users.noreply.github.com>
2019-11-19 22:07:43 -0300

    Relatório

--- relatorio.py
+++ relatorio.py
@@ -24,20 +24,18 @@ def relatar_romaneio(conexao):
 
     listarel = cursor.fetchall()
 
+    now = datetime.now()
+    print("Relatório realizado na data: ", now.day, "/", now.month, "/", now.year, "às", now.hour, "hrs", now.minute, "min", now.second, "sec")
+    print(" ")
+    print("\t\t==== RELATÓRIO DE ROMANEIOS ====")
+    print(" ")
     for u in listarel:
-        if (data >= u[4]):
-            now = datetime.now()
-            print("Relatório realizado na data: ", now.day, "/", now.month, "/", now.year, "às", now.hour, "hrs", now.minute, "min", now.second, "sec")
-            print(" ")
+            if (data == u[4]):
 
-            print("""
-            ==== RELATÓRIO DE ROMANEIOS ====
-
-            Usuário: {}
-            Descrição da peça: {}
-            Cliente: {}
-            Quantidade: {}
-            Data da venda: {}            
-            """.format(u[0], u[1], u[2], u[3], u[4]))
-        else:
-            print("Nenhum registro nessa data!")
+                print("""
+                Usuário: {}
+                Descrição da peça: {}
+                Cliente: {}
+                Quantidade: {}
+                Data da venda: {}            
+                """.format(u[0], u[1], u[2], u[3], u[4]))
